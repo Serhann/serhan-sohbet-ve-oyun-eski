6145cc08b88b407b31093c0a74b8858ae03bce47
Serhann <serhanpw@gmail.com>
2017-06-04 18:45:09 +0300

    Update guildCreate.js

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,20 +4,7 @@ const ayarlar = require('../ayarlar.json');
 
 var prefix = ayarlar.prefix;
 
-const girismesaj = [
-  '**Sohbet ve Oyun BOT sunucunuza eklendi!**',
-  '**Sohbet ve Oyun BOT** sunucunuzdaki insanlara kolaylıklar sağlar.',
-  'Bot Serhan E. tarafından geliştirilmektedir (https://serhan.pw)',
-  'Botumuzun özelliklerini öğrenmek için !yardım komutunu kullanabilirsin.',
-  '**ÖNEMLİ:** Botun kullanması için mod-log kanalı açın ve deneme için',
-  '!uyarı komutunu kullanın.',
-  '',
-  `**Sohbet ve Oyun BOT Resmî Discord Sunucusu** https://discord.gg/GvfuXmE`,
-  `**Sohbet ve Oyun Discord Sunucusu** https://discord.gg/GEeGjnH`
-]
-
 client.on('guildCreate', guild => {
-    const generalChannel = guild.defaultChannel
-    generalChannel.sendMessage(girismesaj)
+	console.log(`${guild.name} adlı sunucuya katıldım!`)
 	client.user.setGame(prefix + 'yardım | ' + client.guilds.size + ' sunucu | ' + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ' kullanıcı');
 })
